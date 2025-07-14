cbc1e95cdbbd5370b1487148ab7f9f2a29c9b684
Suhas-Shende <snshende@outlook.com>
2025-07-14 22:03:41 +0530

    Raising error 404

--- section-03/votee/poll/views.py
+++ section-03/votee/poll/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
+from django.template import loader
+from poll.models import *
 # Create your views here.
+# def index(request):
+#     # top_five_questions=Questions.objects.all()
+#     top_two_questions=Questions.objects.order_by('-pub_date')[:2]  #THis will order the query result
+  
+#     return HttpResponse("-".join([str(question.text) for question in top_two_questions]))  
+
+
+
 def index(request):
-    return HttpResponse("Hello")
+    # top_five_questions=Questions.objects.all()
+    top_two_questions=Questions.objects.order_by('-pub_date')[:2]  #THis will order the query result
+    template=loader.get_template('poll/vote.html')
+    context={'latest_poll':top_two_questions}
+    return HttpResponse(template.render(context,request))  
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+       question=Questions.objects.get(pk=question_id)
+    except Questions.DoesNotExist:
+        raise Http404("404 Page not found")
+
+    context={"question":question}
+    return render(request,'poll/detail.html',context)
 
 
 def results(request, question_id):
