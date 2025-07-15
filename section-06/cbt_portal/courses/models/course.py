754deba068f566d961e46110a41f3841f9d3ee01
Suhas-Shende <snshende@outlook.com>
2025-07-16 02:50:06 +0530

    Getting started with DRF

---
+++ section-06/cbt_portal/courses/models/course.py
@@ -0,0 +1,6 @@
+from django.db.models import *
+
+class Course(Model):
+    name=CharField(max_length=100)
+    code=CharField(max_length=10, unique=True)
+    description=TextField()
---
+++ section-06/cbt_portal/courses/models/exam.py
@@ -0,0 +1,8 @@
+from django.db.models import *
+from courses.models.course import Course
+class Exam(Model):
+    course=ForeignKey(Course, on_delete=CASCADE)
+    title=CharField(max_length=100)
+    total_marks=IntegerField()
+    date=DateTimeField()
+    duration_minutes=IntegerField()
