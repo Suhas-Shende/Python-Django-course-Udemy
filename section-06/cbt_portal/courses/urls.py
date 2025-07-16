e1ae54d41b9b7c8da3d72d454f4a8b891fe5bfb9
Suhas-Shende <snshende@outlook.com>
2025-07-17 02:52:43 +0530

    APIView Class

--- section-06/cbt_portal/courses/urls.py
+++ section-06/cbt_portal/courses/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from courses.views.course_views import *
 urlpatterns=[
 
-    path("all",course_list,name="listallcourses"),
-    path("<int:pk>",course_detail,name="onecoursedetail")
+    # path("all",course_list,name="listallcourses"),
+    path("all",CourseListView.as_view(),name="listallcourses"),
+    # path("<int:pk>",course_detail,name="onecoursedetail")
+    path("<int:pk>",CourseDetailUpdateDelete.as_view(),name="onecoursedetail")
 ]
--- section-06/cbt_portal/courses/views/course_views.py
+++ section-06/cbt_portal/courses/views/course_views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import *
-
+from rest_framework.views import APIView
 @api_view(['GET','POST'])
 def course_list(request):
     if request.method == 'GET':
@@ -47,5 +47,52 @@ def course_detail(request,pk):
             return Response(serializer.data,status=HTTP_201_CREATED)
         return Response(serializer.errors,status=HTTP_400_BAD_REQUEST)
     elif request.method=='DELETE':
+        courses.delete()
+        return Response({'Course':'Deleted Successfully'},status=HTTP_204_NO_CONTENT)
+    
+
+
+class CourseListView(APIView):
+
+    def get(self,request,format=None):
+        courses=Course.objects.all()
+        serializer=CourseSerializer(courses,many=True)
+        # response = {'courses':list(courses.values())}
+        response={
+            'courses':serializer.data
+        }
+    
+        return Response(response,status=HTTP_200_OK) 
+    
+    def post(self,request,format=None):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=HTTP_201_CREATED)
+        return Response(serializer.errors,status=HTTP_400_BAD_REQUEST)
+    
+
+class CourseDetailUpdateDelete(APIView):
+    def get(self,request,pk,format=None):
+        courses=get_object_or_404(Course,pk=pk)
+        serializer=CourseSerializer(courses)
+        # response = {'courses':list(courses.values())}
+        response={
+            'courses':serializer.data
+        }
+    
+        return Response(response,status=HTTP_200_OK)
+    
+    def put(self,request,pk,format=None):
+        courses=get_object_or_404(Course,pk=pk)
+        serializer=CourseSerializer(courses,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response(serializer.data,status=HTTP_201_CREATED)
+        return Response(serializer.errors,status=HTTP_400_BAD_REQUEST)        
+
+    def delete(self,request,pk,format=None):
+        courses=get_object_or_404(Course,pk=pk)
         courses.delete()
         return Response({'Course':'Deleted Successfully'},status=HTTP_204_NO_CONTENT)
